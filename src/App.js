0c5bb90d8172dee74326803e6a8ac61fd5add406
Lucas Intveen <70695669+lucasintveen@users.noreply.github.com>
2021-11-19 16:10:26 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ function App() {
   <div>
     <img src="https://www.itu.dk/kommunikation/Logo/ITU_logo_DK.jpg"/>
     <h3>Hello, hello - I changed something again</h3>
+    <li>
+      <ol>Eins</ol>
+      <ol>Zwei</ol>
+      <ol>Drei</ol>
+    </li>
     <h3>This is fun</h3>
     <img src="https://www.parkworld-online.com/wp-content/uploads/aaaaaaa.png"/>
   </div>
