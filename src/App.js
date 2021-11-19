c1c3950da8f963a8411c69c9b9a40986903c346d
Lucas Intveen <70695669+lucasintveen@users.noreply.github.com>
2021-11-19 15:52:18 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import './App.css';
 
 function App() {
   return (
-  <h2>heogföixu</h2>
-  <h3>Hello, hello - I changed something again</h3>
+  <div>
+    <h2>heogföixu</h2>
+    <h3>Hello, hello - I changed something again</h3>
+  </div>
   );
 }
 
