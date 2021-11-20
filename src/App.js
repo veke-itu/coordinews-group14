e6c56a06f0d0fb9b77951b1f9ef655d80bb967d9
Lucas Intveen <70695669+lucasintveen@users.noreply.github.com>
2021-11-20 15:44:33 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import './App.css';
+import Header from './Components/Header';
 
 function App() {
   return (
   <div>
-    <img src="https://www.itu.dk/kommunikation/Logo/ITU_logo_DK.jpg"/>
-    <h3>Hello, hello - I changed something again</h3>
-    <li>
-      <ol>Eins</ol>
-      <ol>Zwei</ol>
-      <ol>Drei</ol>
-      <ol>Gruppe 14</ol>
-    </li>
-    <h3>This is fun</h3>
-    <img src="https://www.parkworld-online.com/wp-content/uploads/aaaaaaa.png"/>
+    <Header />
   </div>
   );
 }
