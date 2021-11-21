8d0da0a139861570d513063f643c9e126acdc144
Lucas Intveen <70695669+lucasintveen@users.noreply.github.com>
2021-11-21 18:10:47 +0100

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,12 @@
-import React from "react"
+import React from "react";
 
 export default function Header() {
-    return (
-        <header className="header">
-            <h2 className="header--title">Username</h2>
-            <img src="./Images/TID_logo_news-png" className="header--image"/>
-        </header>
+    return(
+        <nav>
+            <img src="/images/profile.png" alt="Profile logo" className="header--profil"/>
+            <img src="/images/news-logo.png" alt="Company logo" className="header--logo"/>
+            <h4 className="">Homepage</h4>
+            <h3 className="">Vera Kempf</h3>
+        </nav>
     )
 }
