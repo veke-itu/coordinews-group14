49ee3348495e5bc71a50c271080fbc27e9d08c1e
Lucas Intveen <70695669+lucasintveen@users.noreply.github.com>
2021-11-22 18:58:49 +0100

    Update Articles.js

---
+++ src/Components/Articles.js
@@ -0,0 +1,8 @@
+export default function Articles() {
+    return(
+        <div>
+            <h1>All Articles for the next newspaper</h1>
+            <h3>Add one if you want to!</h3>
+        </div>
+    )
+}
