54d986899229e71274191f3c259f67e3aa0585d8
Lucas Intveen <70695669+lucasintveen@users.noreply.github.com>
2021-11-22 18:59:06 +0100

    Update Staff.js

---
+++ src/Components/Staff.js
@@ -0,0 +1,8 @@
+export default function Staff() {
+    return(
+        <div>
+            <h1>Here is an overview of the current staff</h1>
+            <h3>Click to find out more!</h3>
+        </div>
+    )
+}
