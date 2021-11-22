dcff840603cd4ab84cbea05dd89031ce04646daf
Lucas Intveen <70695669+lucasintveen@users.noreply.github.com>
2021-11-22 18:58:46 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import NavigationBar from './Navbar';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import { NavigationBar } from "./Components/Navbar";
+import Login from "./Components/Login";
+import SignUp from "./Components/Signup";
+import Ideas from "./Components/Ideas";
+import Articles from "./Components/Articles";
+import Staff from "./Components/Staff";
+import LandingPage from "./Components/LandingPage";
 
 function App() {
   return (
-  <div>
-    <NavigationBar />
-  </div>
+    <div className="App">
+      <HashRouter basename="/">
+        <NavigationBar />
+
+        <Routes>
+          <Route path="signup" element={<SignUp />} />
+          <Route path="login" element={<Login />} />
+          <Route path="ideas" element={<Ideas />} />
+          <Route path="articles" element={<Articles />} />
+          <Route path="staff" element={<Staff />} />
+          <Route path="/" element={<LandingPage />} />
+        </Routes>
+      </HashRouter>
+    </div>
   );
 }
 
