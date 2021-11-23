558bf9d9ad02ce5246e3fd184ccbede426df5cc9
Lucas Intveen <70695669+lucasintveen@users.noreply.github.com>
2021-11-23 11:48:51 +0100

    Update LandingPage.js

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -9,14 +9,22 @@ export default function LandingPage() {
   }
 
   return (
-    <>
-      <h2>
-        <br /> <br />
-        The new software solution for your newspaper is here!!!
-      </h2>
+    <div>
+      <div>
+        <img src="../Images/Coordinews.png"></img>
+      </div>
+      <div>
+        <h3 className="landingPage--heading">
+          Cooperate like never before and concentrate on the things that really matter to you
+        </h3>
+        <h5 className="landingPage--sell">
+          <br /> <br />
+          The software solution for your newspaper is here. Coordinews ensures planning reliability for you and your team from the very first step. With our End to End solution we offer a coordinative approach from the first idea to the finished article for your newspaper, blog or other publication.
+        </h5>
+      </div>
 
       <br />
       <br />
-    </>
+    </div>
   );
 }
