010ef49666dcfefbc88e0dfa6f96354a9dcb1f21
Lucas Intveen <70695669+lucasintveen@users.noreply.github.com>
2021-11-23 11:48:49 +0100

    Update Articles.js

--- src/Components/Articles.js
+++ src/Components/Articles.js
@@ -1,8 +1,40 @@
+import Table from 'react-bootstrap/Table';
+
 export default function Articles() {
     return(
         <div>
             <h1>All Articles for the next newspaper</h1>
             <h3>Add one if you want to!</h3>
+            <Table responsive>
+                <thead>
+                    <tr>
+                    <th>#</th>
+                    {Array.from({ length: 12 }).map((_, index) => (
+                        <th key={index}>Table heading</th>
+                    ))}
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                    <td>1</td>
+                    {Array.from({ length: 12 }).map((_, index) => (
+                        <td key={index}>Table cell {index}</td>
+                    ))}
+                    </tr>
+                    <tr>
+                    <td>2</td>
+                    {Array.from({ length: 12 }).map((_, index) => (
+                        <td key={index}>Table cell {index}</td>
+                    ))}
+                    </tr>
+                    <tr>
+                    <td>3</td>
+                    {Array.from({ length: 12 }).map((_, index) => (
+                        <td key={index}>Table cell {index}</td>
+                    ))}
+                    </tr>
+                </tbody>
+            </Table>
         </div>
     )
 }
