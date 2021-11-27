8ad1117868e6ef660b558b20de9e1aa4c1a8a000
Lucas Intveen <70695669+lucasintveen@users.noreply.github.com>
2021-11-27 15:17:15 +0100

    Playing around with the Navbar
    Still a lot of work left

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -1,5 +1,8 @@
 import Parse from "parse";
 import { useNavigate } from "react-router-dom";
+import LandingPageLogo from "../Images/Coordinews.png";
+import "../App.css";
+import { Container } from "react-bootstrap";
 
 export default function LandingPage() {
   const navigate = useNavigate();
@@ -9,25 +12,28 @@ export default function LandingPage() {
   }
 
   return (
-    <div>
-      <div>
-        <img src="./Images/Coordinews.png" alt=""></img>
-      </div>
-      <div>
-        <br></br>
-        <br></br>
-        <br></br>
-        <h3 className="landingPage--heading">
-          Cooperate like never before and concentrate on the things that really matter to you
+    <div className="splitScreenLanding">
+      <img
+        src={LandingPageLogo}
+        width="auto"
+        height="auto"
+        alt="Coordinews-Logo"
+        className="leftPaneLanding"
+      />
+
+      <div className="rightPaneLanding">
+        <h3>
+          Cooperate like never before - concentrate on the things that really
+          matter to you
         </h3>
-        <h5 className="landingPage--sell">
-          <br /> <br />
-          The software solution for your newspaper is here. Coordinews ensures planning reliability for you and your team from the very first step. With our End to End solution we offer a coordinative approach from the first idea to the finished article for your newspaper, blog or other publication.
+        <h5>
+          The software solution for your newspaper is here. Coordinews ensures
+          planning reliability for you and your team from the very first step.
+          With our End to End solution we offer a coordinative approach from the
+          first idea to the finished article for your newspaper, blog or other
+          publication.
         </h5>
       </div>
-
-      <br />
-      <br />
     </div>
   );
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,10 +1,10 @@
 import { Navbar, Container, Nav } from "react-bootstrap";
 import Parse from "parse";
 import { useNavigate, Link } from "react-router-dom";
-import Image from 'react-bootstrap/Image';
+import Image from "react-bootstrap/Image";
 import NewsLogo from "../Images/Logo_TID.svg";
 //import Breadcrumb from 'react-bootstrap/Breadcrumb';
-import "../App.css"
+import "../App.css";
 
 export function NavigationBar() {
   const navigate = useNavigate();
@@ -17,59 +17,50 @@ export function NavigationBar() {
   }
 
   return (
-      <div className="navBar--general">
-        <Navbar expand="sm">
-          <Container>
-          
-            <Navbar.Brand  as={Link} to="/">
-              <img
-                src={NewsLogo}
-                width="30"
-                height="30"
-                alt="test"
-              />
-            </Navbar.Brand>
+    <Navbar>
+      <Container className="flex--adjustment">
+        <Navbar.Brand as={Link} to="/">
+          <img src={NewsLogo} width="100" height="auto" alt="Coordinews-Logo" />
+        </Navbar.Brand>
 
-              <Navbar.Toggle aria-controls="basic-navbar-nav" />
-              <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto" activeKey={window.location.pathname}>
-                  {!Parse.User.current() && (
-                    <>
-                      <Nav.Link as={Link} to="/signup">
-                        Sign Up
-                      </Nav.Link>
-                      <Nav.Link as={Link} to="/login">
-                        LogIn
-                      </Nav.Link>
-                    </>
-                  )}
+        <Navbar.Toggle />
+        <Navbar.Collapse>
+          <Nav activeKey={window.location.pathname}>
+            {!Parse.User.current() && (
+              <div>
+                <Nav.Link as={Link} to="/signup">
+                  Sign Up
+                </Nav.Link>
+                <Nav.Link as={Link} to="/login">
+                  LogIn
+                </Nav.Link>
+              </div>
+            )}
 
-                  {Parse.User.current() && (
-                    <>
-                      <Nav.Link as={Link} to="/ideas">
-                        Ideas
-                      </Nav.Link>
-                      <Nav.Link as={Link} to="/articles">
-                        Articles
-                      </Nav.Link>
-                      <Nav.Link as={Link} to="/staff">
-                        Staff
-                      </Nav.Link>
+            {Parse.User.current() && (
+              <>
+                <Nav.Link as={Link} to="/ideas">
+                  Ideas
+                </Nav.Link>
+                <Nav.Link as={Link} to="/articles">
+                  Articles
+                </Nav.Link>
+                <Nav.Link as={Link} to="/staff">
+                  Staff
+                </Nav.Link>
 
-                      <Nav.Link onClick={handleSignOut} as={Link} to="/">
-                        Sign Out
-                      </Nav.Link>
+                <Nav.Link onClick={handleSignOut} as={Link} to="/">
+                  Sign Out
+                </Nav.Link>
 
-                      <Nav.Link as={Link} to="/a">
-                        Name
-                      </Nav.Link>
-                      
-                    </>
-                  )}
-                </Nav>
-              </Navbar.Collapse>
-          </Container>
-        </Navbar>
-      </div>
+                <Nav.Link as={Link} to="/a">
+                  Name
+                </Nav.Link>
+              </>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
