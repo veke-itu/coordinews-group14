dcea92c70ebc27672ef40b24a63cd5512c188479
Lucas Intveen <70695669+lucasintveen@users.noreply.github.com>
2021-12-08 14:39:01 +0100

    Minor Adjustments on Popup

--- src/Components/Popup.js
+++ src/Components/Popup.js
@@ -12,11 +12,14 @@ export default function Popup(props) {
           className="close-btn"
           onClick={() => props.setTrigger(false)}
         />
-        {/* <button className="close-btn" onClick={() => props.setTrigger(false)}> */}
-        {/* Close */}
-        {/* </button> */}
+
         <FaCheck className="check--pop" />
-        <button className="OK-btn">Take me to Articles</button>
+        <a href="/#/articles">
+          <button type="submit" className="OK-btn">
+            Take me to Articles
+          </button>
+        </a>
+        {/* <button className="OK-btn">Take me to Articles</button> */}
         {props.children}
       </div>
     </div>
