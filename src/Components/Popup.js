bf6f10475d9f7ab2624fd1c9c2987bfb25571781
Vera Krøigaard Kempf <veke@itu.dk>
2021-12-08 14:24:16 +0100

    Update Popup.js

--- src/Components/Popup.js
+++ src/Components/Popup.js
@@ -5,7 +5,7 @@ export default function Popup(props) {
     return (props.trigger) ? (
         <div className="popup">
             <div className="popup-inner">
-                <button className="close-btn" onClick={() => props.setTrigger(false)} >Close</button>
+                <button className="close-btn" onClick={() => props.setTrigger(false)}>Close</button>
                 <button className="OK-btn">Take me to Aticles</button>
                 {props.children}
             </div>
